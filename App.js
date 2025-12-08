6d040a4aa54dd4c8c5a702b551aaa882445a4aa9
Henry Adams <henryadams@Henrys-MacBook-Air.local>
2025-12-08 12:50:09 +0000

    Initial commit: setup project, App.js and placeholder screens

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WardrobeScreen from './screens/WardrobeScreen';
+import AddItemScreen from './screens/AddItemScreen';
+import OutfitScreen from './screens/OutfitScreen';
+
+
+//Stack Navigator
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Wardrobe">
+        <Stack.Screen name="Wardrobe" component={WardrobeScreen} />
+        <Stack.Screen name="Add Item" component={AddItemScreen} />
+        <Stack.Screen name="Outfit" component={OutfitScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+//Styles
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 24 },
 });
